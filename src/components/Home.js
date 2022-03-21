149e1f967e83d5ed64d2b79d014e6476d7791575
unknown <th7kingdom@gmail.com>
2022-03-21 17:11:43 +0100

    v0.1

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+function Home() {
+  return (
+    <Container>
+        <Section 
+        title="Model S" 
+        description="Order Online"
+        backgroundImg="model-s.jpg"
+        />
+       
+           
+           <Section/>
+        <Section/>
+
+          
+    </Container>
+  )
+}
+
+export default Home
+const Container = styled.div`
+height: 100vh;
+`
